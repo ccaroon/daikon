041357e0487fa8cd4def52f965359ea7722947cd
Craig N. Caroon <craig.caroon@cengage.com>
2022-08-18 11:26:59 -0400

    * Added (empty) builderOptions to vue.config.js * Added icons

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        // options here will be merged with default electron-builder options
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
